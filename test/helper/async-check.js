d3900b4c64bc69ae97a450eeae1f3de92f685277
Shinichi Tomita <shinichi.tomita@gmail.com>
2015-01-08 12:42:40 +0900

    fix dup raising of error

--- test/helper/async-check.js
+++ test/helper/async-check.js
@@ -4,12 +4,7 @@
 Function.prototype.check = function(done) {
   var fn = this;
   return function() {
-    try {
-      fn.apply(this, arguments);
-      return done();
-    } catch(e) {
-      done(e);
-      throw e;
-    }
+    fn.apply(this, arguments);
+    done();
   };
 };
