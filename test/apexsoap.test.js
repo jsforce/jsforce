9c02f4b7729d25d504490cb4cd2e89b73bab2e8f
CV <christophe.vidal.1983@gmail.com>
2016-11-24 10:58:52 +0100

    Implement runTests of the Apex SOAP API

---
+++ test/apexsoap.test.js
@@ -0,0 +1,58 @@
+/*global describe, it, before, after */
+var TestEnv = require('./helper/testenv'),
+    assert = TestEnv.assert;
+
+var async  = require('async'),
+    _      = require('lodash/core'),
+    sf     = require('../lib/jsforce'),
+    config = require('./config/salesforce');
+
+var testEnv = new TestEnv(config);
+
+/**
+ *
+ */
+describe("apexsoap", function() {
+
+  this.timeout(40000); // set timeout to 40 sec.
+
+  var conn = testEnv.createConnection();
+
+  /**
+   *
+   */
+  before(function(done) {
+    this.timeout(600000); // set timeout to 10 min.
+    testEnv.establishConnection(conn, done);
+  });
+
+  var accountId;
+
+  /**
+   *
+   */
+  describe("run tests synchronous", function() {
+    it("should execute one unit test and run successfully", function(done) {
+      var params = {
+        allTests: false,
+        classes: [
+          'JSforceTestUnitTest'
+        ]
+      };
+      conn.apexsoap.runTests(params, function(err, results) {
+        if (err) { throw err; }
+        assert.ok(0 === results.numFailures);
+        assert.ok(1 === results.numTestsRun);
+      }.check(done));
+    });
+  });
+
+ 
+  /**
+   *
+   */
+  after(function(done) {
+    testEnv.closeConnection(conn, done);
+  });
+
+});
