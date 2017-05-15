31344babb38d4cd9de5ce3b1ca8df308855555dc
Shinichi Tomita <shinichi.tomita@gmail.com>
2017-05-15 10:47:53 +0900

    change cache test to match new interface

--- test.es6/cache.test.js
+++ test.es6/cache.test.js
@@ -1,6 +1,6 @@
 import test from './util/ava/ext';
 import { isNumber } from './util';
-import Cache from '../lib/cache';
+import Cache from '../src/cache';
 
 /**
  *
@@ -9,65 +9,43 @@ test.group('cache', (test) => {
   const cache = new Cache();
   let t1, t2, t3, t4, t5; // eslint-disable-line one-var, one-var-declaration-per-line
 
-  const getTime = (callback) => {
-    setTimeout(() => { callback(null, Date.now()); }, 1000);
-  };
+  const getTime = () => new Promise((resolve) => {
+    setTimeout(() => { resolve(Date.now()); }, 200);
+  });
 
-  const getTimeResCached = cache.makeResponseCacheable(getTime, null, { key: 'getTime' });
+  const getTimeWithResCached = cache.createCachedFunction(getTime, null, { key: 'getTime', strategy: 'NOCACHE' });
 
-  test.serial('call response-cached getTime function and return time', async t => (
-    new Promise((resolve, reject) => {
-      getTimeResCached((err, time) => {
-        if (err) { reject(err); return; }
-        t1 = time;
-        t.true(isNumber(t1));
-        resolve();
-      });
-    })
-  ));
+  test.serial('call response-cached getTime function and return time', async (t) => {
+    t1 = await getTimeWithResCached();
+    t.true(isNumber(t1));
+  });
 
-  test.serial('call response-cached function and get different time', async t => (
-    new Promise((resolve, reject) => {
-      getTimeResCached((err, time) => {
-        if (err) { reject(err); return; }
-        t2 = time;
-        t.true(isNumber(t2));
-        t.true(t1 < t2);
-        resolve();
-      });
-    })
-  ));
+  test.serial('call response-cached function and get different time', async (t) => {
+    t2 = await getTimeWithResCached();
+    t.true(isNumber(t2));
+    t.true(t1 < t2);
+  });
 
-  const getTimeCached = cache.makeCacheable(getTime, null, { key: 'getTime' });
+  const getTimeCacheIfHit = cache.createCachedFunction(getTime, null, { key: 'getTime', strategy: 'HIT' });
 
-  test.serial('call cacheable getTime function and get time which equals to latest call result', async t => (
-    new Promise((resolve, reject) => {
-      getTimeCached((err, time) => {
-        if (err) { reject(err); return; }
-        t3 = time;
-        t.true(isNumber(t3));
-        t.true(t3 === t2);
-        resolve();
-      });
-    })
-  ));
+  test.serial('call cacheable getTime function and get time which equals to latest call result', async (t) => {
+    t3 = await getTimeCacheIfHit();
+    t.true(isNumber(t3));
+    t.true(t3 === t2);
+  });
 
-  test.serial('call cached function with no callback and get same time which equals to latest fn call result', (t) => {
-    t4 = getTimeCached();
+  const getTimeCacheImmediate = cache.createCachedFunction(getTime, null, { key: 'getTime', strategy: 'IMMEDIATE' });
+
+  test.serial('call cached function with immediate lookup strategy and get same time which equals to latest fn call result', (t) => {
+    t4 = getTimeCacheImmediate();
     t.true(isNumber(t4));
     t.true(t4 === t3);
   });
 
   test.serial('clear cache and call cache-first function and get time much newer than the latest', async (t) => {
     cache.clear();
-    return new Promise((resolve, reject) => {
-      getTimeCached((err, time) => {
-        if (err) { reject(err); return; }
-        t5 = time;
-        t.true(isNumber(t5));
-        t.true(t4 < t5);
-        resolve();
-      });
-    });
+    t5 = await getTimeCacheIfHit();
+    t.true(isNumber(t5));
+    t.true(t4 < t5);
   });
 });
