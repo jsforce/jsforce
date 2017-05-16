b726498a5ee01ab412d1de0b2abef79e47d3e284
Shinichi Tomita <shinichi.tomita@gmail.com>
2017-05-16 16:59:31 +0900

    separate core module and api module plugins

---
+++ src/api/apex.js
@@ -0,0 +1,102 @@
+/* @flow */
+/**
+ * @file Manages Salesforce Apex REST endpoint calls
+ * @author Shinichi Tomita <shinichi.tomita@gmail.com>
+ */
+import jsforce from '../core';
+import Connection from '../connection';
+import type { HttpRequest } from '../types';
+
+/**
+ * API class for Apex REST endpoint call
+ */
+export default class Apex {
+  _conn: Connection;
+
+  /**
+   *
+   */
+  constructor(conn: Connection) {
+    this._conn = conn;
+  }
+
+  /* @private */
+  _baseUrl() {
+    return `${this._conn.instanceUrl}/services/apexrest`;
+  }
+
+  /**
+   * @private
+   */
+  _createRequestParams(
+    method: string,
+    path: string,
+    body?: Object,
+    options?: Object = {}
+  ): HttpRequest {
+    const params: HttpRequest = {
+      method,
+      url: this._baseUrl() + path,
+      headers: typeof options.headers === 'object' ? options.headers : {},
+    };
+    if (!/^(GET|DELETE)$/i.test(method)) {
+      params.headers['content-type'] = 'application/json';
+    }
+    if (body) {
+      params.body = JSON.stringify(body);
+    }
+    return params;
+  }
+
+  /**
+   * Call Apex REST service in GET request
+   */
+  get(path: string, options?: Object) {
+    return this._conn.request(this._createRequestParams('GET', path, undefined, options));
+  }
+
+  /**
+   * Call Apex REST service in POST request
+   */
+  post(path: string, body?: Object, options?: Object) {
+    const params = this._createRequestParams('POST', path, body, options);
+    return this._conn.request(params);
+  }
+
+  /**
+   * Call Apex REST service in PUT request
+   */
+  put(path: string, body?: Object, options?: Object) {
+    const params = this._createRequestParams('PUT', path, body, options);
+    return this._conn.request(params);
+  }
+
+  /**
+   * Call Apex REST service in PATCH request
+   */
+  patch(path: string, body?: Object, options?: Object) {
+    const params = this._createRequestParams('PATCH', path, body, options);
+    return this._conn.request(params);
+  }
+
+  /**
+   * Call Apex REST service in DELETE request
+   */
+  delete(path: string, options?: Object) {
+    return this._conn.request(this._createRequestParams('DELETE', path, undefined, options));
+  }
+
+  /**
+   * Synonym of Apex#delete()
+   */
+  del = this.delete;
+}
+
+
+/*--------------------------------------------*/
+/*
+ * Register hook in connection instantiation for dynamically adding this API module features
+ */
+jsforce.on('connection:new', (conn) => {
+  conn.apex = new Apex(conn); // eslint-disable-line no-param-reassign
+});
--- src/jsforce.js
+++ src/jsforce.js
@@ -1,6 +1,13 @@
-import Connection from './connection';
-import OAuth2 from './oauth2';
+import Apex from './api/apex';
+import ConnectionCore from './connection';
+import jsforce, { OAuth2, VERSION } from './core';
 
-export { Connection, OAuth2 };
+export { OAuth2, VERSION };
 
-export default { Connection, OAuth2 };
+export class Connection extends ConnectionCore {
+  apex: Apex = new Apex(this);
+}
+
+jsforce.Connection = Connection; // eslint-disable-line import/no-named-as-default-member
+
+export default jsforce;
