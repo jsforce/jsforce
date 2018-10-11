f2070895231f90ef4ef0e230de8dfa380bdef40c
Shinichi Tomita <shinichi.tomita@gmail.com>
2018-10-11 11:42:01 +0900

    temporary remove flow type check for typescript introduce

--- .travis/test.sh
+++ .travis/test.sh
@@ -2,5 +2,6 @@
 if [ "${BROWSER_TEST}" = "true" ]; then
   echo "ava does not support browser yet"
 else
-  npm run lint && npm run flow && npm test
+  # npm run lint && npm run flow && npm test
+  npm run lint && npm test
 fi
