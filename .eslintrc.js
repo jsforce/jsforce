b11bcf890f473b16471cfcfcc63d29f405acca5e
Shinichi Tomita <shinichi.tomita@gmail.com>
2019-07-22 12:07:49 +0900

    change eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: [
+    '@typescript-eslint',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  rules: {
+    'no-unused-vars': 0,
+    '@typescript-eslint/no-unused-vars': 2,
+  },
+};
