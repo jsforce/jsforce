2968f58cc27c197a7ae579ff0a9ab70625a35bae
Shinichi Tomita <shinichi.tomita@gmail.com>
2019-07-23 10:38:37 +0900

    add jest config, power-assert

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  testMatch: ['**/test-ts/**/*.test.ts'],
+  verbose: true,
+  setupFilesAfterEnv: ['./jest.setup.js'],
+};
--- test-ts/helper/browser/webauth.ts
+++ test-ts/helper/browser/webauth.ts
@@ -3,5 +3,8 @@ export default async function authorize(
   _username: string,
   _password: string,
 ) {
-  return {};
+  console.log('not implemented. should not come here!');
+  throw new Error(
+    'automatic OAuth autorization on web browser is not implemented.',
+  );
 }
