334550900cff9a7a02ede1105b167dc0633ff04b
Shinichi Tomita <shinichi.tomita@gmail.com>
2019-12-21 05:43:45 +0900

    add metadata test in jest target

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  testMatch: ['**/test/{connection-*,query,sobject}.test.ts'],
+  testMatch: ['**/test/{connection-*,query,sobject,apex,metadata}.test.ts'],
   verbose: true,
   setupFilesAfterEnv: ['./jest.setup.js'],
 };
