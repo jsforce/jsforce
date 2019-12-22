286e8a3e5f0868bfc5ff318a99ac2bc72b565192
Shinichi Tomita <shinichi.tomita@gmail.com>
2019-12-22 09:14:04 +0900

    change wsdl2schema to dump typescript defs at the same time

--- src/api/util/wsdl2schema.ts
+++ src/api/util/wsdl2schema.ts
@@ -1,7 +1,7 @@
 import fs from 'fs-extra';
 import xml2js from 'xml2js';
-import { convertTypeUsingSchema } from '../../soap';
-import { SoapSchemaType } from '../../types';
+import { castTypeUsingSchema } from '../../soap';
+import { SoapSchemaElementType, SoapSchemaDef } from '../../types';
 import { isMapObject } from '../../util/function';
 
 /**
@@ -19,21 +19,29 @@ const WSDLSimpleTypeSchema = {
   },
 } as const;
 
+const WSDLElementSchema = {
+  $: {
+    name: 'string',
+    type: 'string',
+    minOccurs: '?number',
+    maxOccurs: '?string',
+    nillable: '?boolean',
+  },
+} as const;
+
+const WSDLSequenceSchema = {
+  'xsd:element': ['?', WSDLElementSchema],
+} as const;
+
 const WSDLComplexTypeSchema = {
   $: { name: 'string' },
-  'xsd:sequence': {
+  'xsd:sequence': { '?': WSDLSequenceSchema },
+  'xsd:complexContent': {
     '?': {
-      'xsd:element': [
-        {
-          $: {
-            name: 'string',
-            type: 'string',
-            minOccurs: '?number',
-            maxOccurs: '?string',
-            nillable: '?boolean',
-          },
-        },
-      ],
+      'xsd:extension': {
+        $: { base: 'string' },
+        'xsd:sequence': WSDLSequenceSchema,
+      },
     },
   },
 } as const;
@@ -68,29 +76,39 @@ const WSDLSchema = {
 /**
  *
  */
-type WSDL = SoapSchemaType<typeof WSDLSchema>;
+type WSDL = SoapSchemaElementType<typeof WSDLSchema>;
 
-type WSDLSimpleType = SoapSchemaType<typeof WSDLSimpleTypeSchema>;
+type WSDLElement = SoapSchemaElementType<typeof WSDLElementSchema>;
 
-type WSDLComplexType = SoapSchemaType<typeof WSDLComplexTypeSchema>;
+type WSDLSimpleType = SoapSchemaElementType<typeof WSDLSimpleTypeSchema>;
 
+type WSDLComplexType = SoapSchemaElementType<typeof WSDLComplexTypeSchema>;
+
+/**
+ *
+ */
 function toJsType(xsdType: string, simpleTypes: { [type: string]: string }) {
   switch (xsdType) {
     case 'xsd:boolean':
       return 'boolean';
     case 'xsd:string':
+    case 'xsd:date':
     case 'xsd:dateTime':
+    case 'xsd:time':
     case 'xsd:base64Binary':
       return 'string';
     case 'xsd:int':
     case 'xsd:double':
       return 'number';
+    case 'xsd:anyType':
+      return 'any';
     default: {
       const [ns, type] = xsdType.split(':');
-      if (ns === 'xsd') {
-        return type;
-      } else if (simpleTypes[type]) {
+      if (ns === 'tns' && simpleTypes[type]) {
         return simpleTypes[type];
+      }
+      if (ns === 'xsd' || ns === 'tns') {
+        return type;
       } else {
         return xsdType;
       }
@@ -106,17 +124,41 @@ async function readWSDLFile(filePath: string) {
   const json = await xml2js.parseStringPromise(xmlData, {
     explicitArray: false,
   });
-  return convertTypeUsingSchema(json, WSDLSchema) as WSDL;
+  return castTypeUsingSchema(json, WSDLSchema) as WSDL;
+}
+
+/**
+ *
+ */
+function getTypeInfo(el: WSDLElement, simpleTypes: { [name: string]: string }) {
+  let type = toJsType(el.$.type, simpleTypes);
+  const isArray = el.$.maxOccurs === 'unbounded';
+  const isObject =
+    !isArray &&
+    type !== 'string' &&
+    type !== 'number' &&
+    type !== 'boolean' &&
+    type !== 'any';
+  const nillable = (!isArray && el.$.minOccurs === 0) || el.$.nillable;
+  return isArray
+    ? nillable
+      ? ['?', type]
+      : [type]
+    : nillable
+    ? isObject
+      ? { '?': type }
+      : `?${type}`
+    : type;
 }
 
 /**
  *
  */
 function extractComplexTypes(wsdl: WSDL) {
-  let schemas: { [name: string]: any } = {};
+  let schemas: { [name: string]: SoapSchemaDef } = {};
   const simpleTypes: { [type: string]: string } = {};
   for (const st of wsdl.definitions.types['xsd:schema']['xsd:simpleType']) {
-    const simpleType: WSDLSimpleType = convertTypeUsingSchema(
+    const simpleType: WSDLSimpleType = castTypeUsingSchema(
       st,
       WSDLSimpleTypeSchema,
     );
@@ -124,23 +166,27 @@ function extractComplexTypes(wsdl: WSDL) {
     simpleTypes[simpleType.$.name] = base;
   }
   for (const ct of wsdl.definitions.types['xsd:schema']['xsd:complexType']) {
-    const complexType: WSDLComplexType = convertTypeUsingSchema(
+    const complexType: WSDLComplexType = castTypeUsingSchema(
       ct,
       WSDLComplexTypeSchema,
     );
-    const schema: { [name: string]: any } = {};
+    const schema: {
+      type: string;
+      extends?: string;
+      props: { [name: string]: any };
+    } = {
+      type: complexType.$.name,
+      props: {},
+    };
     for (const el of complexType['xsd:sequence']?.['xsd:element'] ?? []) {
-      let type = toJsType(el.$.type, simpleTypes);
-      const isArray = el.$.maxOccurs === 'unbounded';
-      const nillable = (!isArray && el.$.minOccurs === 0) || el.$.nillable;
-      schema[el.$.name] = isArray
-        ? nillable
-          ? ['?', type]
-          : [type]
-        : nillable
-        ? `?${type}`
-        : type;
-      // console.log(el.$.name, el);
+      schema.props[el.$.name] = getTypeInfo(el, simpleTypes);
+    }
+    if (complexType['xsd:complexContent']) {
+      const extension = complexType['xsd:complexContent']['xsd:extension'];
+      schema.extends = extension.$.base.split(':')[1];
+      for (const el of extension['xsd:sequence']['xsd:element'] ?? []) {
+        schema.props[el.$.name] = getTypeInfo(el, simpleTypes);
+      }
     }
     schemas[complexType.$.name] = schema;
   }
@@ -150,26 +196,12 @@ function extractComplexTypes(wsdl: WSDL) {
 /**
  *
  */
-function resolveSchemaReference(schemas: { [name: string]: any }) {
-  function resolveSchemaRef(v: any): any {
-    if (Array.isArray(v)) {
-      for (let i = 0; i < v.length; i++) {
-        v[i] = resolveSchemaRef(v[i]);
-      }
-    } else if (isMapObject(v)) {
-      for (const k of Object.keys(v)) {
-        v[k] = resolveSchemaRef(v[k]);
-      }
-    } else if (typeof v === 'string' && /^\??tns:/.test(v)) {
-      const nillable = v[0] === '?';
-      const refName = v.replace(/^\??tns:/, '');
-      const ref = schemas[refName];
-      return nillable ? { '?': ref } : ref;
-    }
-    return v;
-  }
-  return resolveSchemaRef(schemas);
-}
+const GENERATED_MESSAGE_COMMENT = `/**
+ * This file is generated from WSDL file by wsdl2schema.ts.
+ * Do not modify directly.
+ * To generate the file, run "ts-node path/to/wsdl2schema.ts path/to/wsdl.xml path/to/schema.ts"
+ */
+`;
 
 /**
  *
@@ -188,8 +220,7 @@ async function dumpSchema(schemas: { [name: string]: any }, outFile: string) {
   return new Promise((resolve, reject) => {
     out.on('error', reject);
     out.on('finish', () => resolve());
-    println('import { SoapSchemaType } from "../../types";');
-    println();
+    println(GENERATED_MESSAGE_COMMENT);
     print('export const ApiSchemas = ');
     writeSchema(schemas);
     println(' as const;');
@@ -200,35 +231,92 @@ async function dumpSchema(schemas: { [name: string]: any }, outFile: string) {
 
   function writeSchema(o: any, indent: number = 0) {
     if (indent > 20) {
-      print('"any"');
+      print("'any'");
       return;
     }
     if (Array.isArray(o)) {
       print('[');
+      let i = 0;
       for (const co of o) {
+        if (i > 0) {
+          print(', ');
+        }
         writeSchema(co, indent);
+        i++;
       }
       print(']');
     } else if (isMapObject(o)) {
-      println('{');
-      for (const name of Object.keys(o)) {
-        const nameId = /^[\w_]+$/.test(name) ? name : `"${name}"`;
-        print(`${nameId}: `, indent + 2);
-        writeSchema(o[name], indent + 2);
-        println(',');
+      const keys = Object.keys(o);
+      if (keys.length > 0) {
+        println('{');
+        for (const name of keys) {
+          const co = o[name];
+          const nameId = /^[\w_$]+$/.test(name) ? name : `'${name}'`;
+          print(`${nameId}: `, indent + 2);
+          writeSchema(co, indent + 2);
+          println(',');
+        }
+        print('}', indent);
+      } else {
+        print('{}');
       }
-      print('}', indent);
-      return;
     } else {
-      print(JSON.stringify(o));
+      print(JSON.stringify(o).replace(/"/g, "'"));
     }
   }
 
-  function writeTypeDefs(schemas: { [name: string]: any }) {
+  function writeTypeDef(o: any, indent: number = 0) {
+    if (typeof o === 'string') {
+      print(o);
+    } else if (isMapObject(o)) {
+      const keys = Object.keys(o);
+      if (keys.length > 0) {
+        println('{');
+        for (const prop of Object.keys(o)) {
+          let value: any = o[prop];
+          let nillable = false;
+          let isArray = false;
+          if (Array.isArray(value)) {
+            isArray = true;
+            const len = value.length;
+            if (len === 2 && value[0] === '?') {
+              nillable = true;
+              value = value[1];
+            } else {
+              value = value[0];
+            }
+          } else if (isMapObject(value)) {
+            if ('?' in value) {
+              value = value['?'];
+            }
+          }
+          if (typeof value === 'string' && value[0] === '?') {
+            value = value.substring(1);
+            nillable = true;
+          }
+          print(`${prop}${nillable ? '?' : ''}: `, indent + 2);
+          writeTypeDef(value, indent + 2);
+          if (isArray) {
+            print('[]');
+          }
+          if (nillable) {
+            print(' | null | undefined');
+          }
+          println(';');
+        }
+        print('}', indent);
+      } else {
+        print('{}');
+      }
+    }
+  }
+
+  function writeTypeDefs(schemas: { [name: string]: SoapSchemaDef }) {
     for (const name of Object.keys(schemas)) {
-      println(
-        `export type ${name} = SoapSchemaType<typeof ApiSchemas.${name}>;`,
-      );
+      const schema = schemas[name];
+      print(`export type ${name} = `);
+      writeTypeDef(schema.props);
+      println(';');
       println();
     }
   }
@@ -239,18 +327,18 @@ async function dumpSchema(schemas: { [name: string]: any }, outFile: string) {
  */
 async function main() {
   const wsdlFilePath = process.argv[2];
+  if (!wsdlFilePath) {
+    console.error('No input WSDL file is specified.');
+    return;
+  }
   const outFilePath = process.argv[3];
-  const targetMeta = process.argv[4].split(',');
+  if (!wsdlFilePath) {
+    console.error('No output typescript schema file is specified.');
+    return;
+  }
   const wsdl = await readWSDLFile(wsdlFilePath);
   const schemas = extractComplexTypes(wsdl);
-  resolveSchemaReference(schemas);
-  const schms: typeof schemas = {};
-  for (const name of Object.keys(schemas)) {
-    if (targetMeta.includes(name)) {
-      schms[name] = schemas[name];
-    }
-  }
-  dumpSchema(schms, outFilePath);
+  dumpSchema(schemas, outFilePath);
 }
 
 main();
