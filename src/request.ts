5e9a4e74328d263b683808db568953009dd5a17e
Shinichi Tomita <shinichi.tomita@gmail.com>
2020-12-07 16:57:24 +0900

    separate redirect handler to helper

--- src/request.ts
+++ src/request.ts
@@ -2,7 +2,11 @@ import EventEmitter from 'events';
 import { Duplex, Readable, Writable } from 'stream';
 import fetch from 'node-fetch';
 import createHttpsProxyAgent from 'https-proxy-agent';
-import { createHttpRequestHandlerStreams } from './request-helper';
+import {
+  createHttpRequestHandlerStreams,
+  isRedirect,
+  performRedirectRequest,
+} from './request-helper';
 import { HttpRequest, HttpRequestOptions } from './types';
 
 /**
@@ -20,72 +24,55 @@ export function setDefaults(defaults_: HttpRequestOptions) {
 /**
  *
  */
-const MAX_REDIRECT_COUNT = 10;
-
-/**
- *
- */
-async function startFetch(
-  req: HttpRequest,
+async function startFetchRequest(
+  request: HttpRequest,
   options: HttpRequestOptions,
   input: Readable | undefined,
   output: Writable,
   emitter: EventEmitter,
-  fetchCount: number = 0,
+  counter: number = 0,
 ) {
   const { httpProxy, followRedirect } = options;
-  const { url, body, ...rreq } = req;
   const agent = httpProxy ? createHttpsProxyAgent(httpProxy) : undefined;
+  const { url, body, ...rrequest } = request;
   const res = await fetch(url, {
-    ...rreq,
-    ...(input && /^(post|put|patch)$/i.test(req.method) ? { body: input } : {}),
+    ...rrequest,
+    ...(input && /^(post|put|patch)$/i.test(request.method)
+      ? { body: input }
+      : {}),
     redirect: 'manual',
     agent,
   });
-  if (followRedirect && fetch.isRedirect(res.status)) {
-    if (fetchCount >= MAX_REDIRECT_COUNT) {
-      emitter.emit('error', new Error('Reached to maximum redirect count'));
-      return;
-    }
-    const redirectUrl = res.headers.get('Location');
-    if (!redirectUrl) {
-      emitter.emit('error', new Error('No redirect URI found'));
-      return;
-    }
-    const getRedirectRequest =
-      typeof followRedirect === 'function'
-        ? followRedirect
-        : () => ({
-            ...rreq,
-            method: 'GET' as const,
-            url: redirectUrl,
-          });
-    const nextReqParams = getRedirectRequest(redirectUrl);
-    if (!nextReqParams) {
-      emitter.emit(
-        'error',
-        new Error('Cannot handle redirect for ' + redirectUrl),
-      );
-      return;
-    }
-    startFetch(
-      nextReqParams,
-      options,
-      undefined,
-      output,
-      emitter,
-      fetchCount + 1,
-    );
-    return;
-  }
   const headers: { [key: string]: any } = {};
   for (const headerName of res.headers.keys()) {
-    headers[headerName] = res.headers.get(headerName);
+    headers[headerName.toLowerCase()] = res.headers.get(headerName);
   }
   const response = {
     statusCode: res.status,
     headers,
   };
+  if (followRedirect && isRedirect(response.statusCode)) {
+    try {
+      performRedirectRequest(
+        request,
+        response,
+        followRedirect,
+        counter,
+        (req) =>
+          startFetchRequest(
+            req,
+            options,
+            undefined,
+            output,
+            emitter,
+            counter + 1,
+          ),
+      );
+    } catch (err) {
+      emitter.emit('error', err);
+    }
+    return;
+  }
   emitter.emit('response', response);
   res.body.pipe(output);
 }
@@ -99,6 +86,6 @@ export default function request(
 ): Duplex {
   const options = { ...defaults, ...options_ };
   const { input, output, stream } = createHttpRequestHandlerStreams(req);
-  startFetch(req, options, input, output, stream);
+  startFetchRequest(req, options, input, output, stream);
   return stream;
 }
