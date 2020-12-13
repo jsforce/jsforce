a079a908deb34537924d9dcd82ef196d34b44b82
Shinichi Tomita <shinichi.tomita@gmail.com>
2020-12-13 19:25:01 +0900

    add chatter test in jest test target

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   testMatch: [
-    '**/test/{connection-*,query,sobject,analytics,apex,bulk,metadata,soap,streaming}.test.ts',
+    '**/test/{connection-*,query,sobject,analytics,apex,bulk,chatter,metadata,soap,streaming}.test.ts',
   ],
   verbose: true,
   setupFilesAfterEnv: ['./jest.setup.js'],
