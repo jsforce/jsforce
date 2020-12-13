42bb1b084fa7907d9ecf0642df9ad53b78c3a4f5
Shinichi Tomita <shinichi.tomita@gmail.com>
2020-12-13 09:46:24 +0900

    add streaming as jest test target

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   testMatch: [
-    '**/test/{connection-*,query,sobject,analytics,apex,bulk,metadata,soap}.test.ts',
+    '**/test/{connection-*,query,sobject,analytics,apex,bulk,metadata,soap,streaming}.test.ts',
   ],
   verbose: true,
   setupFilesAfterEnv: ['./jest.setup.js'],
