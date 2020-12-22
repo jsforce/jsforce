6145f605d42d9bf5dba3d9c1f7273badfff0720b
Shinichi Tomita <shinichi.tomita@gmail.com>
2020-12-23 06:53:46 +0900

    add karma for running test in browser

---
+++ webpack.config.test.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        include: [path.join(__dirname, 'src'), path.join(__dirname, 'test')],
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.js$/,
+        use: ['source-map-loader'],
+        enforce: 'pre',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new webpack.EnvironmentPlugin([
+      'SF_LOGIN_URL',
+      'SF_POOL_CLIENT',
+      'SF_POOL_USERNAME',
+      'SF_POOL_PASSWORD',
+      'SF_USERNAME',
+      'SF_PASSWORD',
+      'SF_OAUTH2_CLIENT_ID',
+      'SF_OAUTH2_CLIENT_SECRET',
+      'SF_OAUTH2_REDIRECT_URI',
+      'SF_AJAX_PROXY_URL',
+      'DEBUG',
+    ]),
+  ],
+};
