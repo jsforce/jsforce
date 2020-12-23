0d1dccf1602152ac440d793a5c1e90f4e85fe64b
Shinichi Tomita <shinichi.tomita@gmail.com>
2020-12-23 20:09:19 +0900

    run karma test single in CI env

--- karma.conf.js
+++ karma.conf.js
@@ -51,7 +51,7 @@ module.exports = function (config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true,
+    singleRun: process.env.CI === 'true',
 
     // Concurrency level
     // how many browser should be started simultaneous
