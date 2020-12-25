dacbf5361381d2b207ad4f359df4ad071905bf72
Shinichi Tomita <shinichi.tomita@gmail.com>
2020-12-25 17:23:18 +0900

    cap the concurrency of browser test execution

--- karma.conf.js
+++ karma.conf.js
@@ -55,7 +55,7 @@ module.exports = function (config) {
 
     // Concurrency level
     // how many browser should be started simultaneous
-    concurrency: Infinity,
+    concurrency: process.env.CI === 'true' ? 5 : 1,
 
     // Webpack
     webpack: webpackConfig,
