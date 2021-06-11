fd6a13771a2f03348bc7d26a1773774312b385d6
Shinichi Tomita <shinichi.tomita@gmail.com>
2021-06-11 17:46:47 +0900

    2.0.0-beta.4

--- dist/jsforce-core.js
+++ dist/jsforce-core.js
@@ -23117,7 +23117,7 @@ exports.callbackify = callbackify;
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-/* harmony default export */ __webpack_exports__["a"] = ('2.0.0-beta.3');
+/* harmony default export */ __webpack_exports__["a"] = ('2.0.0-beta.4');
 
 
 /***/ }),
