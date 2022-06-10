36346fe82830644b4b84524509d21101411c7c9e
Gordon Bockus <gbockus@salesforce.com>
2022-06-10 16:40:25 -0500

    fix: update faye typing to include other common faye client methods

--- typings/faye/index.d.ts
+++ typings/faye/index.d.ts
@@ -10,6 +10,9 @@ declare module 'faye' {
     addExtension(ext: any): void;
     subscribe(channelName: string, listener?: Function): Subscription;
     unsubscribe(channelName: string, subscr: Subscription): void;
+    on(event: string, callback: () => void): void;
+    handshake(callback: () => void): void;
+    disconnect(): void;
   }
   const Faye: { Client: typeof Client };
   export default Faye;
