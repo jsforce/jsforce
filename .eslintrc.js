4b0936f41ca2f77753aa9cd8eed88b7287316478
Willie Ruemmele <willieruemmele@gmail.com>
2024-05-06 14:59:07 -0600

    lint: start linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,7 +27,7 @@ module.exports = {
         arrowParens: 'always',
         singleQuote: true,
         trailingComma: 'all',
-        endOfLine: "auto",
+        endOfLine: 'auto',
       },
     ],
   },
