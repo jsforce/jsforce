5f2ea3a4d875997f1e54d899e717b24fbb66e1f9
Willie Ruemmele <willieruemmele@gmail.com>
2024-05-20 09:43:47 -0600

    test: add more specific imports

--- test/connection-session.test.ts
+++ test/connection-session.test.ts
@@ -1,5 +1,5 @@
 import assert from 'assert';
-import { Connection } from '../src';
+import { Connection } from '../src/connection';
 import { delay } from './util';
 import authorize from './helper/webauth';
 import config from './config';
