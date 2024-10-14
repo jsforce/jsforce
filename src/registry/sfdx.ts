160426335c3d6f8efd1c3244eacb0454e755c988
Cristopher <32661241+Namchee@users.noreply.github.com>
2024-10-14 20:58:06 +0700

    chore(deps): replace strip-ansi with native Node API (#1593)
    Co-authored-by: Cristian Dominguez <6853656+cristiand391@users.noreply.github.com>

--- src/registry/sfdx.ts
+++ src/registry/sfdx.ts
@@ -1,5 +1,5 @@
 import { exec } from 'child_process';
-import stripAnsi from 'strip-ansi';
+import { stripVTControlCharacters } from 'util';
 import Connection from '../connection';
 import { Registry, ConnectionConfig, ClientConfig } from './types';
 import { Schema } from '../types';
@@ -78,7 +78,7 @@ export class SfdxRegistry implements Registry {
         }
       });
     });
-    const body = stripAnsi(buf.toString());
+    const body = stripVTControlCharacters(buf.toString());
     let ret: SfdxCommandOutput;
     try {
       ret = JSON.parse(body) as SfdxCommandOutput;
