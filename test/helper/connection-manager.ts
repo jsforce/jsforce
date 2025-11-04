0555c88f205f7a6663b7db0cd621c320415e8523
Cristian Dominguez <cdominguez@salesforce.com>
2025-11-04 13:33:25 -0300

    chore: drop soap login, use access token for test auth

--- test/helper/connection-manager.ts
+++ test/helper/connection-manager.ts
@@ -22,12 +22,16 @@ export default class ConnectionManager {
 
   async establishConnection<S extends Schema>(conn: Connection<S>) {
     const config = this._config;
-    const username = config.username;
 
-    if (username && config.password) {
-      await conn.login(username, config.password);
-    } else {
-      throw new Error('Set SF_USERNAME and SF_PASSWORD.');
-    }
+    // Establish a connection using the access token from the env var (retrieved from sf CLI).
+    //
+    // There are some other OAuth flow availables but each has its own quirks:
+    // * Client Credentials: requires an ECA to deploy when setting up scratch org
+    // * Username-Password: this flows doesn't work when running tests in parallel because each new session ID request invalidates old ones
+    // * Web Server: too flaky/slow for test runs in parallel
+    conn._establish({
+      accessToken: config.accessToken,
+      instanceUrl: config.loginUrl,
+    })
   }
 }
